d54013bc4a144ebf4ac95aea5e8dae6cd9a1616a
diego <dbarriapaiva@gmail.com>
2022-01-03 09:33:31 -0300

    cursor supports any integer type now

--- src/cursor.zig
+++ src/cursor.zig
@@ -6,35 +6,35 @@ const term = lib.term;
 const utils = lib.utils;
 
 /// Moves cursor to `x` column and `y` row
-pub inline fn goTo(x: u16, y: u16) []const u8 {
+pub inline fn goTo(x: anytype, y: anytype) []const u8 {
     // i guess is ok with this size for now
     var buf: [30]u8 = undefined;
     return fmt.bufPrint(&buf, "\x1b[{d};{d}H", .{ y, x }) catch unreachable;
 }
 
 /// Moves cursor up `y` rows
-pub inline fn goUp(y: u16) []const u8 {
+pub inline fn goUp(y: anytype) []const u8 {
     // i guess is ok with this size for now
     var buf: [30]u8 = undefined;
     return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
 }
 
 /// Moves cursor down `y` rows
-pub inline fn goDown(y: u16) []const u8 {
+pub inline fn goDown(y: anytype) []const u8 {
     // i guess is ok with this size for now
     var buf: [30]u8 = undefined;
     return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
 }
 
 /// Moves cursor left `x` columns
-pub inline fn goLeft(x: u16) []const u8 {
+pub inline fn goLeft(x: anytype) []const u8 {
     // i guess is ok with this size for now
     var buf: [30]u8 = undefined;
     return fmt.bufPrint(&buf, "\x1b[{d}D", .{x}) catch unreachable;
 }
 
 /// Moves cursor right `x` columns
-pub inline fn goRight(x: u16) []const u8 {
+pub inline fn goRight(x: anytype) []const u8 {
     // i guess is ok with this size for now
     var buf: [30]u8 = undefined;
     return fmt.bufPrint(&buf, "\x1b[{d}C", .{x}) catch unreachable;
