ebd4186a22406510f4458a6bf40bddc53745d376
xyaman <dbarriapaiva@gmail.com>
2022-03-23 14:16:00 -0300

    `cursor` module changed
    - Now previous functions are in `cursor.print` module.
    - `cursor` now uses writers

--- src/cursor.zig
+++ src/cursor.zig
@@ -5,61 +5,105 @@ const lib = @import("main.zig");
 const term = lib.term;
 const utils = lib.utils;
 
+pub const print = struct {
+
+    /// Moves cursor to `x` column and `y` row
+    pub inline fn goTo(x: anytype, y: anytype) []const u8 {
+        // i guess is ok with this size for now
+        var buf: [30]u8 = undefined;
+        return fmt.bufPrint(&buf, "\x1b[{d};{d}H", .{ y, x }) catch unreachable;
+    }
+
+    /// Moves cursor up `y` rows
+    pub inline fn goUp(y: anytype) []const u8 {
+        // i guess is ok with this size for now
+        var buf: [30]u8 = undefined;
+        return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
+    }
+
+    /// Moves cursor down `y` rows
+    pub inline fn goDown(y: anytype) []const u8 {
+        // i guess is ok with this size for now
+        var buf: [30]u8 = undefined;
+        return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
+    }
+
+    /// Moves cursor left `x` columns
+    pub inline fn goLeft(x: anytype) []const u8 {
+        // i guess is ok with this size for now
+        var buf: [30]u8 = undefined;
+        return fmt.bufPrint(&buf, "\x1b[{d}D", .{x}) catch unreachable;
+    }
+
+    /// Moves cursor right `x` columns
+    pub inline fn goRight(x: anytype) []const u8 {
+        // i guess is ok with this size for now
+        var buf: [30]u8 = undefined;
+        return fmt.bufPrint(&buf, "\x1b[{d}C", .{x}) catch unreachable;
+    }
+
+    /// Hide the cursor
+    pub inline fn hide() []const u8 {
+        return utils.comptimeCsi("?25l", .{});
+    }
+
+    /// Show the cursor
+    pub inline fn show() []const u8 {
+        return utils.comptimeCsi("?25h", .{});
+    }
+
+    /// Save cursor position
+    pub inline fn save() []const u8 {
+        return utils.comptimeCsi("u", .{});
+    }
+
+    /// Restore cursor position
+    pub inline fn restore() []const u8 {
+        return utils.comptimeCsi("s", .{});
+    }
+};
+
 /// Moves cursor to `x` column and `y` row
-pub inline fn goTo(x: anytype, y: anytype) []const u8 {
-    // i guess is ok with this size for now
-    var buf: [30]u8 = undefined;
-    return fmt.bufPrint(&buf, "\x1b[{d};{d}H", .{ y, x }) catch unreachable;
+pub fn goTo(writer: anytype, x: anytype, y: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "{d};{d}H", .{ y, x });
 }
 
 /// Moves cursor up `y` rows
-pub inline fn goUp(y: anytype) []const u8 {
-    // i guess is ok with this size for now
-    var buf: [30]u8 = undefined;
-    return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
+pub fn goUp(writer: anytype, y: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "{d}A", .{y});
 }
 
 /// Moves cursor down `y` rows
-pub inline fn goDown(y: anytype) []const u8 {
-    // i guess is ok with this size for now
-    var buf: [30]u8 = undefined;
-    return fmt.bufPrint(&buf, "\x1b[{d}A", .{y}) catch unreachable;
+pub fn goDown(writer: anytype, y: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "{d}B", .{y});
 }
 
 /// Moves cursor left `x` columns
-pub inline fn goLeft(x: anytype) []const u8 {
-    // i guess is ok with this size for now
-    var buf: [30]u8 = undefined;
-    return fmt.bufPrint(&buf, "\x1b[{d}D", .{x}) catch unreachable;
+pub fn goLeft(writer: anytype, x: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "{d}D", .{x});
 }
 
 /// Moves cursor right `x` columns
-pub inline fn goRight(x: anytype) []const u8 {
-    // i guess is ok with this size for now
-    var buf: [30]u8 = undefined;
-    return fmt.bufPrint(&buf, "\x1b[{d}C", .{x}) catch unreachable;
+pub fn goRight(writer: anytype, x: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "{d}C", .{x});
 }
 
 /// Hide the cursor
-pub inline fn hide() []const u8 {
-    return utils.comptimeCsi("?25l", .{});
+pub fn hide(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "?25l", .{});
 }
 
 /// Show the cursor
-pub inline fn show() []const u8 {
-    return utils.comptimeCsi("?25h", .{});
+pub fn show(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "?25h", .{});
 }
 
 /// Save cursor position
-pub inline fn save() []const u8 {
-    return utils.comptimeCsi("u", .{});
+pub fn save(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "u", .{});
 }
 
 /// Restore cursor position
-pub inline fn restore() []const u8 {
-    return utils.comptimeCsi("s", .{});
-}
-
-pub inline fn getPos() []const u8 {
-    return utils.comptimeCsi("6n", .{});
+pub fn restore(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ "s", .{});
 }
