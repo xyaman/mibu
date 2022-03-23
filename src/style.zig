1163e7a86e24d383ca37787e4f725689f21056ad
xyaman <dbarriapaiva@gmail.com>
2022-03-23 14:00:31 -0300

    `style` module changed
    - Now previous functions are in `style.print` module.
    - `style` now uses writers

--- src/style.zig
+++ src/style.zig
@@ -1,36 +1,125 @@
 const utils = @import("main.zig").utils;
+const std = @import("std");
+
+pub const print = struct {
+
+    /// Returns the ANSI sequence as a []const u8
+    pub const reset = utils.comptimeCsi("0m", .{});
+
+    /// Returns the ANSI sequence to set bold mode
+    pub const bold = utils.comptimeCsi("1m", .{});
+    pub const no_bold = utils.comptimeCsi("22m", .{});
+
+    /// Returns the ANSI sequence to set dim mode
+    pub const dim = utils.comptimeCsi("2m", .{});
+    pub const no_dim = utils.comptimeCsi("22m", .{});
+
+    /// Returns the ANSI sequence to set italic mode
+    pub const italic = utils.comptimeCsi("3m", .{});
+    pub const no_italic = utils.comptimeCsi("23m", .{});
+
+    /// Returns the ANSI sequence to set underline mode
+    pub const underline = utils.comptimeCsi("4m", .{});
+    pub const no_underline = utils.comptimeCsi("24m", .{});
+
+    /// Returns the ANSI sequence to set blinking mode
+    pub const blinking = utils.comptimeCsi("5m", .{});
+    pub const no_blinking = utils.comptimeCsi("25m", .{});
+
+    /// Returns the ANSI sequence to set reverse mode
+    pub const reverse = utils.comptimeCsi("7m", .{});
+    pub const no_reverse = utils.comptimeCsi("27m", .{});
+
+    /// Returns the ANSI sequence to set hidden/invisible mode
+    pub const invisible = utils.comptimeCsi("8m", .{});
+    pub const no_invisible = utils.comptimeCsi("28m", .{});
+
+    /// Returns the ANSI sequence to set strikethrough mode
+    pub const strikethrough = utils.comptimeCsi("9m", .{});
+    pub const no_strikethrough = utils.comptimeCsi("29m", .{});
+};
 
 /// Returns the ANSI sequence as a []const u8
-pub const reset = utils.comptimeCsi("0m", .{});
+pub fn reset(writer: anytype) !void {
+    return std.fmt.format(writer, utils.reset_all, .{});
+}
 
 /// Returns the ANSI sequence to set bold mode
-pub const bold = utils.comptimeCsi("1m", .{});
-pub const no_bold = utils.comptimeCsi("22m", .{});
+pub fn bold(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_bold, .{});
+}
+
+/// Returns the ANSI sequence to unset bold mode
+pub fn noBold(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_bold, .{});
+}
 
 /// Returns the ANSI sequence to set dim mode
-pub const dim = utils.comptimeCsi("2m", .{});
-pub const no_dim = utils.comptimeCsi("22m", .{});
+pub fn dim(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_dim, .{});
+}
+
+/// Returns the ANSI sequence to unset dim mode
+pub fn noDim(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_dim, .{});
+}
 
 /// Returns the ANSI sequence to set italic mode
-pub const italic = utils.comptimeCsi("3m", .{});
-pub const no_italic = utils.comptimeCsi("23m", .{});
+pub fn italic(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_italic, .{});
+}
+
+/// Returns the ANSI sequence to unset italic mode
+pub fn noItalic(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_italic, .{});
+}
 
 /// Returns the ANSI sequence to set underline mode
-pub const underline = utils.comptimeCsi("4m", .{});
-pub const no_underline = utils.comptimeCsi("24m", .{});
+pub fn underline(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_underline, .{});
+}
+
+/// Returns the ANSI sequence to unset underline mode
+pub fn noUnderline(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_underline, .{});
+}
 
 /// Returns the ANSI sequence to set blinking mode
-pub const blinking = utils.comptimeCsi("5m", .{});
-pub const no_blinking = utils.comptimeCsi("25m", .{});
+pub fn blinking(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_blinking, .{});
+}
+
+/// Returns the ANSI sequence to unset blinking mode
+pub fn noBlinking(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_blinking, .{});
+}
 
 /// Returns the ANSI sequence to set reverse mode
-pub const reverse = utils.comptimeCsi("7m", .{});
-pub const no_reverse = utils.comptimeCsi("27m", .{});
+pub fn reverse(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_reverse, .{});
+}
+
+/// Returns the ANSI sequence to unset reverse mode
+pub fn noReverse(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_reverse, .{});
+}
 
 /// Returns the ANSI sequence to set hidden/invisible mode
-pub const invisible = utils.comptimeCsi("8m", .{});
-pub const no_invisible = utils.comptimeCsi("28m", .{});
+pub fn hidden(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_invisible, .{});
+}
 
-/// Returns the ANSI sequence to set strikethrough mode
-pub const strikethrough = utils.comptimeCsi("9m", .{});
-pub const no_strikethrough = utils.comptimeCsi("29m", .{});
+/// Returns the ANSI sequence to unset hidden/invisible mode
+pub fn noHidden(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_invisible, .{});
+}
+
+/// Returns the ansi sequence to set strikethrough mode
+pub fn strikethrough(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_strikethrough, .{});
+}
+
+/// Returns the ansi sequence to unset strikethrough mode
+pub fn noStrikethrough(writer: anytype) !void {
+    return std.fmt.format(writer, utils.style_no_strikethrough, .{});
+}
--- src/utils.zig
+++ src/utils.zig
@@ -35,6 +35,41 @@ pub const clear_line_to_cursor = "1K";
 /// Clear entire line
 pub const clear_line = "2K";
 
+/// Returns the ANSI sequence as a []const u8
+pub const reset = "0m";
+
+/// Returns the ANSI sequence to set bold mode
+pub const style_bold = "1m";
+pub const style_no_bold = "22m";
+
+/// Returnstyle_s the ANSI sequence to set dim mode
+pub const style_dim = "2m";
+pub const style_no_dim = "22m";
+
+/// Returnstyle_s the ANSI sequence to set italic mode
+pub const style_italic = "3m";
+pub const style_no_italic = "23m";
+
+/// Returnstyle_s the ANSI sequence to set underline mode
+pub const style_underline = "4m";
+pub const style_no_underline = "24m";
+
+/// Returnstyle_s the ANSI sequence to set blinking mode
+pub const style_blinking = "5m";
+pub const style_no_blinking = "25m";
+
+/// Returnstyle_s the ANSI sequence to set reverse mode
+pub const style_reverse = "7m";
+pub const style_no_reverse = "27m";
+
+/// Returnstyle_s the ANSI sequence to set hidden/invisible mode
+pub const style_invisible = "8m";
+pub const style_no_invisible = "28m";
+
+/// Returnstyle_s the ANSI sequence to set strikethrough mode
+pub const style_strikethrough = "9m";
+pub const style_no_strikethrough = "29m";
+
 pub inline fn comptimeCsi(comptime fmt: []const u8, args: anytype) []const u8 {
     const str = "\x1b[" ++ fmt;
     return std.fmt.comptimePrint(str, args);
