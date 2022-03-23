a9568ee15eb227598ce65f1fa29daf9d4d16a6a2
xyaman <xyaman@Diegos-MacBook-Pro.local>
2022-03-23 13:34:47 -0300

    `clear` module changed
    - Now previous functions are in `clear.print` module.
    - `clear` now uses writers

--- src/clear.zig
+++ src/clear.zig
@@ -7,20 +7,53 @@ const std = @import("std");
 const lib = @import("main.zig");
 const utils = lib.utils;
 
+pub const print = struct {
+
+    /// Clear from cursor until end of screen
+    pub const screen_from_cursor = utils.comptimeCsi("0J", .{});
+
+    /// Clear from cursor to beginning of screen
+    pub const screen_to_cursor = utils.comptimeCsi("1J", .{});
+
+    /// Clear all screen
+    pub const all = utils.comptimeCsi("2J", .{});
+
+    /// Clear from cursor to end of line
+    pub const line_from_cursor = utils.comptimeCsi("0K", .{});
+
+    /// Clear start of line to the cursor
+    pub const line_to_cursor = utils.comptimeCsi("1K", .{});
+
+    /// Clear entire line
+    pub const line = utils.comptimeCsi("2K", .{});
+};
+
 /// Clear from cursor until end of screen
-pub const screen_from_cursor = utils.comptimeCsi("0J", .{});
+pub fn screenFromCursor(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_screen_from_cursor, .{});
+}
 
 /// Clear from cursor to beginning of screen
-pub const screen_to_cursor = utils.comptimeCsi("1J", .{});
+pub fn screenToCursor(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_screen_to_cursor, .{});
+}
 
 /// Clear all screen
-pub const all = utils.comptimeCsi("2J", .{});
+pub fn all(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_all, .{});
+}
 
 /// Clear from cursor to end of line
-pub const line_from_cursor = utils.comptimeCsi("0K", .{});
+pub fn line_from_cursor(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_line_from_cursor, .{});
+}
 
 /// Clear start of line to the cursor
-pub const line_to_cursor = utils.comptimeCsi("1K", .{});
+pub fn line_to_cursor(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_line_to_cursor, .{});
+}
 
 /// Clear entire line
-pub const line = utils.comptimeCsi("2K", .{});
+pub fn entire_line(writer: anytype) !void {
+    return std.fmt.format(writer, utils.csi ++ utils.clear_line, .{});
+}
--- src/utils.zig
+++ src/utils.zig
@@ -17,6 +17,24 @@ pub const bg_rgb = "48;2;";
 /// Sequence to reset color and style
 pub const reset_all = "0m";
 
+/// Sequence to clear from cursor until end of screen
+pub const clear_screen_from_cursor = "0J";
+
+/// Sequence to clear from beginning to cursor.
+pub const clear_screen_to_cursor = "1J";
+
+/// Sequence to clear all screen
+pub const clear_all = "2J";
+
+/// Clear from cursor to end of line
+pub const clear_line_from_cursor = "0K";
+
+/// Clear start of line to the cursor
+pub const clear_line_to_cursor = "1K";
+
+/// Clear entire line
+pub const clear_line = "2K";
+
 pub inline fn comptimeCsi(comptime fmt: []const u8, args: anytype) []const u8 {
     const str = "\x1b[" ++ fmt;
     return std.fmt.comptimePrint(str, args);
