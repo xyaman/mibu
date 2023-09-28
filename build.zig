4e213a9f703a250dce2fcfa2d2510b327580dcac
FunkyEgg <funkyeggdev@proton.me>
2023-09-28 11:45:41 +1000

    Fixed build.zig to my knowledge

--- build.zig
+++ build.zig
@@ -1,33 +1,44 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const target = b.standardTargetOptions(.{});
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("mibu", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const mibu_module = b.addModule("mibu", .{ .source_file = .{ .path = "src/main.zig" }});
 
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
     // examples
-    const color = b.addExecutable("color", "examples/color.zig");
-    color.setTarget(target);
-    color.addPackagePath("mibu", "src/main.zig");
+    // const color = b.addExecutable("color", "examples/color.zig");
+    const color = b.addExecutable(.{
+        .name = "color",
+        .root_source_file = .{ .path = "examples/color.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    color.addModule("mibu", mibu_module);
+    // b.installArtifact(color);
 
     const color_step = b.step("color", "Run color example");
-    color_step.dependOn(&color.run().step);
+    color_step.dependOn(&color.step);
 
-    const event = b.addExecutable("event", "examples/event.zig");
-    event.setTarget(target);
-    event.addPackagePath("mibu", "src/main.zig");
+    // const event = b.addExecutable("event", "examples/event.zig");
+    const event = b.addExecutable(.{
+        .name = "event",
+        .root_source_file = .{ .path = "examples/event.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    event.addModule("mibu", mibu_module);
+    // b.installArtifact(event);
 
     const event_step = b.step("event", "Run event example");
-    event_step.dependOn(&event.run().step);
+    event_step.dependOn(&event.step);
 }
